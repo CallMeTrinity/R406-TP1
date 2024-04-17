149db5307cb740dfe3486916a9827da5f14d1f98
CallMeTrinity <anteur.ap@gmail.com>
2024-04-17 17:56:18 +0200

    Init

--- www/js/TP_api.js
+++ www/js/TP_api.js
@@ -1,4 +1,38 @@
 "use strict";
 
-const API_URL = 'https://dummyjson.com/'; // URL de base de l'API
+const API_URL = 'https://dummyjson.com/product'; // URL de base de l'API
 
+
+async function fetchData(){
+    await fetch(API_URL)
+        .then(response => response.json())
+        .then(products => {
+            console.log("=>(TP_api.js:11) products", products);
+            const productList = document.getElementById('productList');
+            const categoryList = document.getElementById('categoryList');
+
+            products.products.forEach(product => {
+                const li = document.createElement('li');
+                li.className = 'list-group-item d-flex justify-content-between';
+                li.textContent = product.title;
+
+                const icon = document.createElement('i');
+                icon.className = 'bi bi-trash del-prod';
+                icon.addEventListener('click', function() {
+                    productList.removeChild(li);
+                });
+
+                li.appendChild(icon);
+                productList.appendChild(li);
+            });
+
+        })
+}
+
+function update(){
+
+}
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData();
+
+})
