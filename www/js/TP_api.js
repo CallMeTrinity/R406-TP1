9e73ecf4dc873cc1a9282fe0166e0fcf0c20824e
CallMeTrinity <anteur.ap@gmail.com>
2024-04-18 12:12:52 +0200

    api

--- www/js/TP_api.js
+++ www/js/TP_api.js
@@ -1,37 +1,70 @@
 "use strict";
 
-const API_URL = 'https://dummyjson.com/product'; // URL de base de l'API
+const API_URL = "https://dummyjson.com"; // URL de base de l'API
 
+async function fetchData(url) {
+  const response = await fetch(url);
+  try {
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-async function fetchData(){
-    await fetch(API_URL)
-        .then(response => response.json())
-        .then(data => {
-            console.log("=>(TP_api.js:11) products", data);
-            const productList = document.getElementById('productList');
-            const categoryList = document.getElementById('categoryList');
+document.addEventListener("DOMContentLoaded", async () => {
+  let categoriesBtn;
+  try {
+    const jsonData = await fetchData(`${API_URL}/products/categories`);
+    const listCategory = document.getElementById("categoryList");
 
-            data.products.forEach(product => {
-                const li = document.createElement('li');
-                li.className = 'list-group-item d-flex justify-content-between';
-                li.textContent = product.title;
+    jsonData.forEach((category) => {
+      const element = `<button class="btn btn-outline-secondary text-start" type="button" data-category="${category}">${category}</button>`;
+      listCategory.insertAdjacentHTML("afterbegin", element);
+      categoriesBtn = document.querySelectorAll("[data-category]");
+    });
+  } catch (error) {
+    console.error("Error fetching or processing data:", error);
+  }
 
-                const icon = document.createElement('i');
-                icon.className = 'bi bi-trash del-prod';
-                icon.addEventListener('click', function() {
-                    productList.removeChild(li);
-                });
+  categoriesBtn.forEach((btn) => {
+    btn.addEventListener("click", async () => {
+      let deleteBtn;
+      try {
+        const category = btn.getAttribute("data-category");
+        const products = await fetchData(
+          `${API_URL}/products/category/${category}`
+        );
+        const productList = document.getElementById("productList");
+        productList.innerHTML = "";
+        console.log(products.products);
 
-                li.appendChild(icon);
-                productList.appendChild(li);
+        products.products.forEach((product) => {
+          const element = `<li class="list-group-item d-flex justify-content-between">${product.title} <i class="bi bi-trash del-prod" data-id=${product.id}></i></li>`;
+          productList.insertAdjacentHTML("afterbegin", element);
+        });
+        deleteBtn = document.querySelectorAll("[data-id]");
+        deleteBtn.forEach((btn) => {
+            console.log(btn)
+            btn.addEventListener("click", async () => {
+              try {
+                const id = btn.getAttribute("data-id");
+                fetch(`${API_URL}/products/${id}`, {
+                  method: "DELETE",
+                })
+                  .then((res) => res.json())
+                  .then(console.log);
+              } catch (error) {
+                console.error("Error fetching or processing data:", error);
+              }
             });
+          });
+    
+      } catch (error) {
+        console.error("Error fetching or processing data:", error);
+      }
+    });
+  });
 
-        })
-}
-
-function update(){
-
-}
-document.addEventListener('DOMContentLoaded', () => {
-    fetchData();
-})
+  
+});
